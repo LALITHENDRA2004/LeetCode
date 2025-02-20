2cefcc6c328a00799328cc2eac82e3d0f155ce19
Lalithendra <119876880+LALITHENDRA2004@users.noreply.github.com>
2025-02-20 12:18:01 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (55.88%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+void find(string &s, int &ind, string &ans, vector<string> &nums) {
+    if (ans != "") return; // If we've already found an answer, return
+    if (s.size() == nums.size()) {
+        // Check if the current string is not in nums
+        if (ind >= nums.size() || nums[ind] != s) {
+            ans = s; // Assign the missing string to ans
+            return;
+        }
+        ind++; // Move to the next string in nums
+        return;
+    }
+    s += "0";
+    find(s, ind, ans, nums);
+    s.pop_back();
+    s += "1";
+    find(s, ind, ans, nums);
+    s.pop_back();
+}
+    string findDifferentBinaryString(vector<string>& nums) {
+        sort(nums.begin(), nums.end()); // Sort the input array
+    int ind = 0;
+    string s = "", ans = "";
+    find(s, ind, ans, nums); // Start generating binary strings
+    return ans;
+    }
+};
