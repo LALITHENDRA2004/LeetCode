9d86dcee5a46c41822ac04ec5d724702be507dd4
Lalithendra <119876880+LALITHENDRA2004@users.noreply.github.com>
2025-02-20 17:16:53 +0530

    Time: 47 ms (14.24%) | Memory: 63.1 MB (12.12%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
-void find(string s, int &ind, string &ans, vector<string> &nums) {
-    if (ans != "") return; // If we've already found an answer, return
-    if (s.size() == nums.size()) {
-        // Check if the current string is not in nums
-        if (ind >= nums.size() || nums[ind] != s) {
-            ans = s; // Assign the missing string to ans
+    void find(string s, int &ind, string &ans, vector<string> &nums) {
+        if(ans != "") return;
+        if(s.size() == nums.size()) {
+            if(ind < nums.size() && nums[ind] != s) {
+                ans = s;
+            }
+            else ind++;
             return;
         }
-        ind++; // Move to the next string in nums
-        return;
-    }
-    s += "0";
-    find(s, ind, ans, nums);
-    s.pop_back();
-    s += "1";
-    find(s, ind, ans, nums);
-    //s.pop_back();
-}
+        s += "0";
+        find(s, ind, ans, nums);
+        s.pop_back();
+        s += "1";
+        find(s, ind, ans, nums);
+    } 
     string findDifferentBinaryString(vector<string>& nums) {
-        sort(nums.begin(), nums.end()); // Sort the input array
-    int ind = 0;
-    string s = "", ans = "";
-    find(s, ind, ans, nums); // Start generating binary strings
-    return ans;
+        sort(nums.begin(),nums.end());
+        for(auto i : nums) cout << i << " ";
+        int ind = 0;
+        string s = "", ans = "";
+        find(s, ind, ans, nums);
+        if(ans == "") {
+            ans = nums[nums.size() - 1];
+            for(int i = nums.size() - 1 ; i >= 0 ; i--) {
+                if(ans[i] == '0') {
+                    ans[i] = '1';
+                    break;
+                }
+            }
+        }
+        return ans;
     }
 };
