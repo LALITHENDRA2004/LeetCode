317d6d9422c8a170e245938c01e68b483521eeff
Lalithendra <119876880+LALITHENDRA2004@users.noreply.github.com>
2025-02-24 22:53:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (74.63%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
-    int find(int i, int j, vector<vector<int>> &dp) {
-        if(i == 0 && j == 0) return 1;
-        if(i < 0 || j < 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int top = find(i - 1, j, dp);
-        int left = find(i, j - 1, dp);
-        return dp[i][j] = top + left;
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int> (n, 1));
+        vector<int> prev2(n, 1);
+        vector<int> prev1(n, 1);
         for(int i = 1 ; i < m ; i++) {
             for(int j = 1 ; j < n ; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]; 
+                prev1[j] = prev2[j] + prev1[j - 1];
+                prev2 = prev1;
             }
         }
-        return dp[m - 1][n - 1];
+        return prev1[n - 1];
+        
     }
 };
