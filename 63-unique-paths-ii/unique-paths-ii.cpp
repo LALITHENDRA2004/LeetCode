f7b8b6c2ecc97be8dbe31f928489a88b6db9baba
Lalithendra <119876880+LALITHENDRA2004@users.noreply.github.com>
2025-02-25 08:15:42 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (28.98%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     int find(int row, int col, vector<vector<int>> &grid, vector<vector<int>> &dp) {
-        cout << row << " " << col << endl;
-        if(row == 0 && col == 0) {
-            if(grid[row][col] == 1) return 0;
-            return 1;
-        }
         if(row < 0 || col < 0 || grid[row][col] == 1) return 0;
+        if(row == 0 && col == 0) return 1;
         if(dp[row][col] != -1) return dp[row][col];
         int top = 0, left = 0;
         top = find(row - 1, col, grid, dp);
@@ -17,6 +13,8 @@ public:
         int rows = obstacleGrid.size(), cols = obstacleGrid[0].size();
         vector<vector<int>> dp(rows, vector<int> (cols, -1));
         return find(rows - 1, cols - 1, obstacleGrid, dp);
+
+
 
     }
 };
