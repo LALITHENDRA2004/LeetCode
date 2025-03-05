55f41c6ef53832b06048da8526b377815e529420
Lalithendra <119876880+LALITHENDRA2004@users.noreply.github.com>
2025-03-05 18:07:58 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (5.63%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,17 +1,27 @@
 class Solution {
-public: 
+public:
     #define ll long long
-    int find(int row, int i, vector<vector<int>> &t, vector<vector<int>> &dp) {
-        if(row >= t.size()) return 0;
-        if(i < 0 || i >= t[row].size()) return INT_MAX;
-        if(dp[row][i] != -1) return dp[row][i];
-        ll left = t[row][i] + find(row + 1, i, t, dp);
-        ll right = t[row][i] + find(row + 1, i + 1, t, dp);
-        return dp[row][i] = min(left, right);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
-        int rows = triangle.size(), cols = triangle[rows - 1].size();
-        vector<vector<int>> dp(rows, vector<int> (cols, -1));
-        return find(0, 0, triangle, dp);
+        ll rows = triangle.size() - 1, cols = triangle[rows].size() - 1;
+        vector<vector<ll>> dp(rows + 1, vector<ll> (cols + 1, INT_MAX));
+
+        dp[0][0] = triangle[0][0];
+        ll ans = INT_MAX;
+
+        for(ll i = 1 ; i <= rows ; i++) {
+            for(ll j = 0 ; j < triangle[i].size() ; j++) {
+                ll mini = INT_MAX;
+
+                ll one = triangle[i][j] + dp[i-1][j], two = INT_MAX;
+                if(j-1 >= 0) two = triangle[i][j] + dp[i-1][j-1];
+
+                dp[i][j] = min(one, two);
+
+                if(i == rows) {
+                    ans = min(ans, dp[i][j]);
+                }
+            }
+        }
+        return ans == INT_MAX ? triangle[0][0] : ans;
     }
 };
