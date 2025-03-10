3f6455b35d4747839ef42132f9d3cfe341003f68
Lalithendra <119876880+LALITHENDRA2004@users.noreply.github.com>
2025-03-10 18:06:54 +0530

    Time: 135 ms (41.39%) | Memory: 100.4 MB (38.41%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    bool find(int i, int tar, vector<int> &arr, vector<vector<int>> &dp) {
-        if(tar == 0) return true;
-        if(i == 0) {
-            if(tar == arr[i]) return true;
-            return false;
-        }
-        if(dp[i][tar] != -1) return dp[i][tar];
-        int pick = 0;
-        if(arr[i] <= tar) pick = find(i - 1, tar - arr[i], arr, dp);
-        int notPick = find(i - 1, tar, arr, dp);
-        return dp[i][tar] = pick || notPick;
-    }
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         for(int i : nums) sum += i;
 
         if(sum % 2 != 0) return false;
-        vector<vector<int>> dp(nums.size(), vector<int> ((sum / 2) + 1, -1));
-        return find(nums.size() - 1, sum / 2, nums, dp);
+        vector<vector<int>> dp(nums.size(), vector<int> ((sum / 2) + 1, 0));
+
+        for(int i = 0 ; i < nums.size() ; i++) {
+            dp[i][0] = true;
+        }
+
+        if(nums[0] <= (sum / 2)) dp[0][nums[0]] = true;
+
+        for(int i = 1 ; i < nums.size() ; i++) {
+            for(int t = 1 ; t <= (sum / 2) ; t++) {
+                bool pick = false;
+                if(nums[i] <= t) pick = dp[i - 1][t - nums[i]];
+                bool notPick = dp[i - 1][t];
+
+                dp[i][t] = pick || notPick;
+            }
+        }
+
+        return dp[nums.size() - 1][(sum / 2)];
     }
 };
