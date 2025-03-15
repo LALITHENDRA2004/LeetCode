c4fe093b53460a0b264dd4f828444a80e2994bef
Lalithendra <119876880+LALITHENDRA2004@users.noreply.github.com>
2025-03-15 22:51:31 +0530

    Time: 327 ms (12.51%) | Memory: 157.3 MB (14.10%) - LeetSync

--- 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.cpp
+++ 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
     vector<int> findOriginalArray(vector<int>& changed) {
+        if(changed.size() % 2 != 0) return {};
+        map<int, int> cnt;
+        for(int i : changed) cnt[i]++;
 
-        multiset<int> ms;
-        int cnt = 0;
-        for(int i : changed) {
-            if(i != 0) ms.insert(i);
-            else cnt++;
-        }
-        if(cnt % 2 != 0) return {};
-
-        sort(changed.begin(), changed.end());
         vector<int> ans;
+        sort(changed.begin(), changed.end());
         for(int i : changed) {
-            auto it1 = ms.find(i);
-            int doub = i * 2;
-
-            auto it2 = ms.find(doub);
-            if(it1 != ms.end() && it2 != ms.end()) {
-                ans.push_back(i);
-                ms.erase(it1);
-                ms.erase(it2);
+           if(i != 0 && cnt[i] > 0) {
+                if(cnt[2 * i] > 0) {
+                    cnt[i]--;
+                    cnt[2 * i]--;
+                    ans.push_back(i);
+                }
             }
         }
-        if(ms.size() != 0) return {};
+        if(cnt[0] % 2 != 0) return {};
+        for(int i = 0 ; i < cnt[0] / 2 ; i++) {
+            ans.push_back(0);
+        }
+        if(ans.size() == changed.size() / 2) return ans;
+        return {};
 
-        for(int i = 0 ; i < cnt / 2 ; i++) ans.push_back(0);
-        return ans;
-        
+
     }
 };
