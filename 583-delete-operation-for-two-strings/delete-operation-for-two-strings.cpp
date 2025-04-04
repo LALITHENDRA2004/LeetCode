74dd3a0924597c10bff722a7210b2d51c99707da
Lalithendra <119876880+LALITHENDRA2004@users.noreply.github.com>
2025-04-04 21:32:17 +0530

    Time: 12 ms (50.58%) | Memory: 12 MB (81.96%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int lcs(string text1, string text2) {
+        int n = text1.size(), m = text2.size();
+        vector<int> prev(m), curr(m);
+        
+        if(text1[0] == text2[0]) {
+            prev[0] = 1;
+        }
+
+        for(int i = 1 ; i < m ; i++) {
+            if(text1[0] == text2[i]) prev[i] = max(1, prev[i - 1]);
+            else prev[i] = prev[i - 1]; 
+        } 
+
+        for(int i = 1 ; i < n ; i++) {
+
+            if(text1[i] == text2[0]) curr[0] = max(1, prev[0]);
+            else curr[0] = prev[0];
+            
+            for(int j = 1 ; j < m ; j++) {
+                if(text1[i] == text2[j]) {
+                    curr[j] = 1 + prev[j - 1];
+                }
+                else {
+                    int left  = prev[j];
+                    int right = curr[j - 1];
+
+                    curr[j] = max(left, right);
+                }
+            }
+
+            prev = curr;
+        }
+
+        return prev[m - 1];
+    }
+    int minDistance(string word1, string word2) {
+        int l = lcs(word1, word2);
+        int ans = word1.size() - l + word2.size() - l;
+        return ans;
+    }
+};
