96ca3d07d42cb45a5af39362429daffac5a15a7a
Lalithendra <119876880+LALITHENDRA2004@users.noreply.github.com>
2025-04-06 09:17:05 +0530

    Time: 8 ms (46.57%) | Memory: 13.2 MB (42.49%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    int find(int i, int j, string &s, string &t, vector<vector<int>> &dp) {
-        if(i == 0) return j;
-        if(j == 0) return i;
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s[i - 1] == t[j - 1]) {
-            return find(i - 1, j - 1, s, t, dp);
-        }
-
-        int insert = 1 + find(i, j - 1, s, t, dp);
-        int deletee = 1 + find(i - 1, j, s, t, dp);
-        int replace = 1 + find(i - 1, j - 1, s, t, dp);
-
-        int ans = min(insert, min(deletee, replace));
-        
-        return dp[i][j] = ans;
-    }
     int minDistance(string word1, string word2) {
         int n = word1.size(), m = word2.size();
 
         vector<vector<int>> dp(n + 1, vector<int> (m + 1, -1));
-        return find(n, m, word1, word2, dp);
+        for(int i = 0 ; i <= n ; i++) {
+            dp[i][0] = i;
+        }
+        for(int j = 0 ; j <= m ; j++) {
+            dp[0][j] = j;
+        }
+
+        for(int i = 1 ; i <= n ; i++) {
+            for(int j = 1 ; j <= m ; j++) {
+                if(word1[i - 1] == word2[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+                else {
+                    dp[i][j] = 1 + min(dp[i][j - 1], min(dp[i - 1][j], dp[i - 1][j - 1]));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
