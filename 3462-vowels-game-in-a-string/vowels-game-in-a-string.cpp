e1dfbf26d1a663fe9f6ca81c5c58621f7dda27c0
Lalithendra <119876880+LALITHENDRA2004@users.noreply.github.com>
2025-09-12 18:04:06 +0530

    Time: 3 ms (87.13%) | Memory: 18.9 MB (79.48%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        for(char i : s) {
+            if(i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u') return true;
+        }
+        return false; 
+    }
+};
